76c44256d3c271ce4cc226bb58faf9d3b1383919
BeansWalker <beanswalker.io@gmail.com>
2018-11-13 14:28:22 -0500

    Ultima versión estable

--- Views/category.php
+++ Views/category.php
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+// Ultima modificación con funcionamiento correcto
 require_once './header.php';
 ?>
 <!--Contenido-->
